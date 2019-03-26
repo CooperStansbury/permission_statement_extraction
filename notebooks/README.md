a0449b4ede9e739b4b703df7d0338520f9d513d2
CooperStansbury <cstansbu@umich.edu>
2019-03-26 09:10:53 -0400

    move towards modules, update notebooks

---
+++ notebooks/README.md
@@ -0,0 +1,5 @@
+# Notebooks
+These are Jupyter Notebooks used to carry out the analysis and perform analysis and supporting tasks. Most notebooks are written in Python 3.7.0 with no regard for backwards compatibility.
+
+## Modules
+For convenience, and to make reproducing analysis steps across notebooks easier, there are `.py` modules located in this directory. They contain function definitions that may be used in multiple notebooks. These are also written in Python 3.7.0 with no regard for backwards compatibility.
---
+++ notebooks/clean_data.py
@@ -0,0 +1,59 @@
+
+import re
+import spacy
+
+# load once, call many
+nlp_larg = spacy.load('en_core_web_lg')
+
+
+### --------------------- 'APPLY' FUNCTIONS --------------------- ###
+
+def minimalTextCleaning(row, field):
+    """ perform text processing on raw data to new field """
+
+    # force encoding
+    encoded_text = row[field].encode(encoding = 'ascii',errors = 'replace')
+    decoded_text = encoded_text.decode(encoding='ascii',errors='strict')
+    remove_funky_chars = str(decoded_text).replace("?", " ")
+    lower_case = str(remove_funky_chars).lower().strip()
+
+    # strip redundant whitespace
+    cleaned_text = re.sub(' +', ' ', lower_case)
+
+
+    # strip signature lines
+    cleaned_text = cleaned_text.replace("_", "")
+
+    return cleaned_text
+
+
+def getDocObjects(row, field):
+    """ return spacy doc object from a text field """
+
+    doc = nlp_larg(str(row[field]).lower())
+
+    return doc
+
+
+def getSentenceList(row, field):
+    """ return list of sentences from doc object field;
+    each item will be token span """
+
+    return list(row[field].sents)
+
+
+def cleanSents(row, field):
+    """ perform minor text cleaning on all sents """
+
+    text = str(row[field]).lower()
+    clean_text = re.sub('[^A-Za-z0-9]+', ' ', text).strip()
+    return clean_text
+
+
+def convertAnnotationtoBinary(row, field):
+    """ return binary (0,1), where 1 = permission_statement """
+
+    if str(row[field]).__contains__('NON'):
+        return 0
+    else:
+        return 1
