90a18ecc9237b4235e1dcb6859a21a09d46e097d
CooperStansbury <cstansbu@umich.edu>
2019-03-26 16:19:54 -0400

    rename and move BERT scripts

---
+++ scripts/predict_bert.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+BERT_BASE_DIR=../bert/uncased_L-12_H-768_A-12
+BERT_DIR=../bert
+OUTPUT_DIR=../bert_output
+
+python3 $BERT_DIR/run_classifier.py \
+--task_name=cola \
+--do_predict=true \
+--data_dir=$BERT_DIR/data \
+--vocab_file=$BERT_BASE_DIR/vocab.txt \
+--bert_config_file=$BERT_BASE_DIR/bert_config.json \
+--init_checkpoint=$TRAINED_CLASSIFIER \
+--max_seq_length=128 \
+--output_dir=$OUTPUT_DIR/
--- scripts/train_bert.sh
+++ scripts/train_bert.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-export BERT_BASE_DIR=./bert/uncased_L-12_H-768_A-12
+BERT_BASE_DIR=../bert/uncased_L-12_H-768_A-12
+BERT_DIR=../bert
+OUTPUT_DIR=../bert_output
 
-python3 bert/run_classifier.py \
+python3 $BERT_DIR/run_classifier.py \
 --task_name=cola \
 --do_train=true \
 --do_eval=true \
---data_dir=./bert/data \
+--data_dir=$BERT_DIR/data \
 --vocab_file=$BERT_BASE_DIR/vocab.txt \
 --bert_config_file=$BERT_BASE_DIR/bert_config.json \
 --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
@@ -14,4 +16,4 @@ python3 bert/run_classifier.py \
 --train_batch_size=32 \
 --learning_rate=2e-5 \
 --num_train_epochs=3.0 \
---output_dir=./bert_output/
+--output_dir=$OUTPUT_DIR/
