126d1bcf4c72a5f3b3e601d2ac0d8d65fe057a63
CooperStansbury <cstansbu@umich.edu>
2019-04-01 18:37:17 -0400

    updated performance evaluation

--- scripts/train_bert.sh
+++ scripts/train_bert.sh
@@ -9,6 +9,7 @@ python3 $BERT_DIR/run_classifier.py \
 --task_name=cola \
 --do_train=true \
 --do_eval=true \
+--do_predict=true \
 --data_dir=$DATA \
 --vocab_file=$BERT_BASE_DIR/vocab.txt \
 --bert_config_file=$BERT_BASE_DIR/bert_config.json \
